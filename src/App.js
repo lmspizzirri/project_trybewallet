40b79fd0a65bc2556d5e5436c4e3349d59ec2f36
Lucas <lm.spizzirri@gmail.com>
2023-02-15 19:56:22 -0300

    Req 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
-function App() {
-  return <div>Helo, TrybeWallet!</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
+      </Switch>
+    );
+  }
 }
-
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
-    return <div>Login</div>;
+    const emailPattern = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+    const passwordLength = 5;
+    const { email, password } = this.state;
+    const { history } = this.props;
+
+    return (
+      <div>
+        <h1>Login</h1>
+        <input
+          type="email"
+          data-testid="email-input"
+          name="email"
+          placeholder="Email"
+          onChange={ this.handleChange }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          name="password"
+          placeholder="Senha"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="submit"
+          disabled={ !(emailPattern.test(email) && password.length > passwordLength) }
+          onClick={ () => {
+            history.push('/carteira');
+          } }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
 export default Login;
+
+Login.propTypes = {
+  history: PropTypes.func,
+}.isRequired;
