aa87f4082bc62fc7e0fd3e417751cd6d50eda6de
Lucas <lm.spizzirri@gmail.com>
2023-02-28 00:50:49 -0300

    Req 6 tentativa numero 287

--- src/components/Table.js
+++ src/components/Table.js
@@ -17,12 +17,12 @@ class Table extends Component {
           <tr>
             <th>Descrição</th>
             <th>Tag</th>
-            <th>Método de Pagamento</th>
+            <th>Método de pagamento</th>
             <th>Valor</th>
             <th>Moeda</th>
             <th>Câmbio utilizado</th>
             <th>Valor convertido</th>
-            <th>Moeda de Conversão</th>
+            <th>Moeda de conversão</th>
             <th>Editar/Excluir</th>
           </tr>
         </thead>
